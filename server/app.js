ede1a9d8a0ca000c06683244260de37d2f12decb
Matthew Keymer <mastercyrax@hotmail.com>
2016-03-13 19:15:40 -0500

    Updated port for Heroku

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ var app = express();
 var bodyParser = require("body-parser");
 var index = require("./routes/index");
 
-app.set("port", (process.env.PORT || 5000));
+app.set("port", (process.env.PORT || 3000));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
